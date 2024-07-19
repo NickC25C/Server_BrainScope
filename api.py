383da6bc2369614c3281fff85400632b422c5288
Dagasfi <daniel.gaspar98@gmail.com>
2024-07-19 11:12:04 +0200

    [UPD]: Server with local ipv4

--- api.py
+++ api.py
@@ -1,6 +1,17 @@
 from flask import Flask, Response
 from module_preprocess import pre_process
 
+import socket
+
+def get_ip():
+    local_hostname = socket.gethostname()
+    ip_addresses = socket.gethostbyname_ex(local_hostname)[2]
+    filtered_ips = [ip for ip in ip_addresses if not ip.startswith("127.")]
+    return filtered_ips[:1][0]
+
+HOST = get_ip()
+PORT = "5000"
+
 app = Flask(__name__)
 
 @app.route('/process_data', methods=['POST'])
@@ -12,5 +23,5 @@ def process_data():
     except Exception as e:
         return Response(f"An error occurred: {str(e)}", status=500, mimetype='text/plain')
 
-if __name__ == '__main__':
-    app.run(debug=True) 
+if __name__ == '__main__':  
+    app.run(host= HOST, port=PORT, debug=True) 
